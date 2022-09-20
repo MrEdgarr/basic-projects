d1a1c14f4964daf01629725cba810e1ba2a85fc9
MrEdgarr <nqhuy11101@gmal.com>
2022-09-20 21:31:42 +0700

    asd

--- src/App.js
+++ src/App.js
@@ -1,54 +1,57 @@
 import './App.css';
 
 function App() {
+  const basic_projects = [
+    {
+      name: 'Menu',
+      image: '/images/menu.png',
+      url: 'https://basic-project-menu.netlify.app/',
+    },
+    {
+      name: 'Slider',
+      image: '/images/slider.png',
+      url: 'https://basic-project-slide.netlify.app/',
+    },
+    {
+      name: 'Grocety Bud',
+      image: '/images/grocery-bud.png',
+      url: 'https://basic-project-grocery-bud.netlify.app/',
+    },
+    {
+      name: 'Cart',
+      image: '/images/cart.png',
+      url: 'https://basic-project-cart.netlify.app/',
+    },
+    {
+      name: 'Search',
+      image: '/images/search.png',
+      url: 'https://basic-project-search.netlify.app/',
+    },
+  ];
+  const handleClickUrl = (url) => {
+    window.open(url)
+  }
   return (
     <>
-      <section classname="section">
-        <div classname="title">
+      <section className='section'>
+        <div className='title'>
           <h2>Basic Projects</h2>
-          <div classname="title-underline" />
+          <div className='title-underline'></div>
         </div>
-        <div classname="section-center">
-          <a href="https://basic-project-menu.netlify.app/" target="_blank">
-            <div classname="img-container">
-              <img src="./images/menu.png" />
-            </div>
-            <footer>
-              <h5>Menu</h5>
-            </footer>
-          </a>
-          <a href="https://basic-project-slide.netlify.app/" target="_blank">
-            <div classname="img-container">
-              <img src="./images/slider.png" alt="" />
-            </div>
-            <footer>
-              <h5>Slider</h5>
-            </footer>
-          </a>
-          <a href="https://basic-project-grocery-bud.netlify.app/" target="_blank">
-            <div classname="img-container">
-              <img src="./images/grocery-bud.png" alt="" />
-            </div>
-            <footer>
-              <h5>Grocety Bud</h5>
-            </footer>
-          </a>
-          <a href="https://basic-project-cart.netlify.app/" target="_blank">
-            <div classname="img-container">
-              <img src="./images/cart.png" alt="" />
-            </div>
-            <footer>
-              <h5>Cart</h5>
-            </footer>
-          </a>
-          <a href="https://basic-project-search.netlify.app/" target="_blank">
-            <div classname="img-container">
-              <img src="./images/search.png" alt="" />
-            </div>
-            <footer>
-              <h5>Search</h5>
-            </footer>
-          </a>
+        <div className='section-center'>
+          {
+            basic_projects.map((value, index) => {
+              const { name, url, image } = value;
+              return <a key={index} onClick={() => handleClickUrl(url)}>
+                <div className='img-container'>
+                  <img src={image} alt="" />
+                </div>
+                <footer>
+                  <h5>{name}</h5>
+                </footer>
+              </a>
+            })
+          }
         </div>
       </section>
     </>
